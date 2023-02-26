da15e0197b2c5a70aef517002c4d3cf0c60a44c8
Brynner03 <bventura03@hotmail.com>
2023-02-26 18:10:19 -0500

    Formatted code

--- Data Structures/Strings.js
+++ Data Structures/Strings.js
@@ -1,167 +1,166 @@
-
 const isArrayUnique = (str) => {
-    if (str.length == 1) {
-        return true
+  if (str.length == 1) {
+    return true;
+  }
+  if (str.length == 0) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 1; j < str.length; j++) {
+      if (str[i] !== str[j]) {
+        j++;
+      } else {
+        return false;
+      }
     }
-    if (str.length == 0) {
-        return false
-    }
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 1; j < str.length; j++) {
-            if (str[i] !== str[j]) {
-                j++
-            } else {
-                return false
-            }
-        }
-        i++
-    }
-    return true
-}
+    i++;
+  }
+  return true;
+};
 
 const isPermutation = (str1, str2) => {
-    // Base case
-    if (str1.length !== str2.length || str1 == null || str2 == null) {
-        return false
+  // Base case
+  if (str1.length !== str2.length || str1 == null || str2 == null) {
+    return false;
+  }
+  // Get the map of the first string
+  let charMap = getMap(str1);
+  // Check strB characters and occurrences on the computed map.
+  for (let i = 0; i < str1.length; i++) {
+    let charB = str2[i];
+    if (charMap[charB] !== undefined && charMap[charB] >= 1) {
+      charMap[charB]--;
+    } else {
+      return false;
     }
-    // Get the map of the first string
-    let charMap = getMap(str1)
-    // Check strB characters and occurrences on the computed map.
-    for (let i = 0; i < str1.length; i++) {
-        let charB = str2[i]
-        if (charMap[charB] !== undefined && charMap[charB] >= 1) {
-            charMap[charB] --
-        } else {
-            return false
-        }
-    }
-    return true
-}
+  }
+  return true;
+};
 
 const getMap = (str) => {
-    let map = {}
-    for (let i = 0 ; i < str.length; i++) {
-        let currentChar = str[i]
-        if (!map[currentChar]) {
-            map[currentChar] = 1
-        } else {
-            map[currentChar] += 1
-        }
+  let map = {};
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+    if (!map[currentChar]) {
+      map[currentChar] = 1;
+    } else {
+      map[currentChar] += 1;
     }
-    return map
-}
+  }
+  return map;
+};
 
 // URLify
 const cleanWhiteSpaces = (str) => {
-    let cleanedString = str.replace(/\s/g, '%20')
-    return cleanedString
-}
+  let cleanedString = str.replace(/\s/g, "%20");
+  return cleanedString;
+};
 
 // Palindrome Permutation
 const isPalindromePermutation = (str) => {
-    let map = {}
-    let normString = str.toLowerCase()
-    for (let i = 0; i < normString.length; i++) {
-        let currentChar = normString[i]   
-        if (currentChar !== ' ') {
-            map[currentChar] = map[currentChar] ? map[currentChar] += 1 : 1
-        }
+  let map = {};
+  let normString = str.toLowerCase();
+  for (let i = 0; i < normString.length; i++) {
+    let currentChar = normString[i];
+    if (currentChar !== " ") {
+      map[currentChar] = map[currentChar] ? (map[currentChar] += 1) : 1;
     }
-    let hasOddFreq = false
-    for (const char in map) {
-        let freq = map[char]
-        if (freq %2 !== 0) {
-            if (hasOddFreq) {
-                return false
-            } else {
-                hasOddFreq = true
-            }
-        }
+  }
+  let hasOddFreq = false;
+  for (const char in map) {
+    let freq = map[char];
+    if (freq % 2 !== 0) {
+      if (hasOddFreq) {
+        return false;
+      } else {
+        hasOddFreq = true;
+      }
     }
-    return true
-}
+  }
+  return true;
+};
 
 const oneAway = (str1, str2) => {
-    const map = {}
-    // Loop through the first string and map them
-    for (let i = 0; i < str1.length; i++) {
-        let currChar = str1[i]
-        if (map[currChar]) {
-            map[currChar] += 1
-        } else {
-            map[currChar] = 1
-        }
+  const map = {};
+  // Loop through the first string and map them
+  for (let i = 0; i < str1.length; i++) {
+    let currChar = str1[i];
+    if (map[currChar]) {
+      map[currChar] += 1;
+    } else {
+      map[currChar] = 1;
     }
-    // Loop through the second string and check if it exists in the map. 
-    // If it does, decrement it. 
-    // If it doesn't, store the value.
-    // If it is equal to 0, remove it from the map.
-    for (let j = 0; j < str2.length; j++) {
-        let str2Char = str2[j]
-        if (map[str2Char]) {
-            map[str2Char] --
-        } else {
-            map[str2Char] = 1
-        }
-        if (map[str2Char] == 0) {
-            delete map[str2Char]
-        }
+  }
+  // Loop through the second string and check if it exists in the map.
+  // If it does, decrement it.
+  // If it doesn't, store the value.
+  // If it is equal to 0, remove it from the map.
+  for (let j = 0; j < str2.length; j++) {
+    let str2Char = str2[j];
+    if (map[str2Char]) {
+      map[str2Char]--;
+    } else {
+      map[str2Char] = 1;
     }
-    // If the length of values in the map is more than 1, return false.
-    if (Object.values(map).length >= 2) {
-        return false
-    } 
-        return true
-}
+    if (map[str2Char] == 0) {
+      delete map[str2Char];
+    }
+  }
+  // If the length of values in the map is more than 1, return false.
+  if (Object.values(map).length >= 2) {
+    return false;
+  }
+  return true;
+};
 
 const compressString = (str) => {
-    let compString = ""
-    let count = 1
-    for (let i = 0; i < str.length; i ++) {
-        if (str[i] == str[i + 1]) {
-            count++
-        } else {
-            compString += str[i] + count
-            count = 1
-        }
-    }
-    if (str.length <= compString.length) {
-        return str
+  let compString = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) {
+      count++;
     } else {
-        return compString
+      compString += str[i] + count;
+      count = 1;
     }
-}
+  }
+  if (str.length <= compString.length) {
+    return str;
+  } else {
+    return compString;
+  }
+};
 
 const rotateMatrix = (arr) => {
-    let left = 0
-    let right = arr.length - 1
-    while( left  < right) {
-        for (let i = 0; i < right - left; i++) {
-            let top = left 
-            let bottom = right
-            // Save the top left variable
-            let topLeft = arr[top][left + i] 
-            // Move the bottom left into the top left
-            arr[top][left + i] = arr[bottom - i][left]
-            // Move bottom right into bottom left
-            arr[bottom - i][left] = arr[bottom] [right - i]
-            // Move top right into bottom right
-            arr[bottom][right - i] = arr[top + i][right]
-            // Move top left into top right
-            arr[top + i][right] = topLeft
-        }
-        right--
-        left++
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    for (let i = 0; i < right - left; i++) {
+      let top = left;
+      let bottom = right;
+      // Save the top left variable
+      let topLeft = arr[top][left + i];
+      // Move the bottom left into the top left
+      arr[top][left + i] = arr[bottom - i][left];
+      // Move bottom right into bottom left
+      arr[bottom - i][left] = arr[bottom][right - i];
+      // Move top right into bottom right
+      arr[bottom][right - i] = arr[top + i][right];
+      // Move top left into top right
+      arr[top + i][right] = topLeft;
     }
-    return arr
-}
+    right--;
+    left++;
+  }
+  return arr;
+};
 
 module.exports = {
-    isArrayUnique,
-    isPermutation,
-    cleanWhiteSpaces,
-    isPalindromePermutation,
-    oneAway,
-    compressString,
-    rotateMatrix
-}
+  isArrayUnique,
+  isPermutation,
+  cleanWhiteSpaces,
+  isPalindromePermutation,
+  oneAway,
+  compressString,
+  rotateMatrix,
+};
--- Tests/Strings.test.js
+++ Tests/Strings.test.js
@@ -1,85 +1,100 @@
-import * as exports from '../Data Structures/Strings'
-import { describe, it, expect } from 'vitest';
+import * as exports from "../Data Structures/Strings";
+import { describe, it, expect } from "vitest";
 
+describe("isArrayUnique()", () => {
+  it("Should return true if all values in the string are unique", () => {
+    const str1 = "lo";
+    const str2 = "loo";
+    const str3 = "l";
 
-describe('isArrayUnique()', () => {
-    it('Should return true if all values in the string are unique', () => {
-        const str1 = 'lo'
-        const str2 = 'loo'
-        const str3 = 'l'
+    expect(exports.isArrayUnique(str1)).toEqual(true);
+    expect(exports.isArrayUnique(str2)).toEqual(false);
+    expect(exports.isArrayUnique(str3)).toEqual(true);
+  });
+});
 
-        expect(exports.isArrayUnique(str1)).toEqual(true)
-        expect(exports.isArrayUnique(str2)).toEqual(false)
-        expect(exports.isArrayUnique(str3)).toEqual(true)
-    })
-})
+describe("isPermutation()", () => {
+  it("Should return true if all characters from the first string are equal to the second string", () => {
+    const str1 = "abcd";
+    const str2 = "dabc";
 
-describe('isPermutation()', () => {
-    it('Should return true if all characters from the first string are equal to the second string', () => {
-        const str1 = 'abcd'
-        const str2 = 'dabc'
+    const str3 = "buie";
 
-        const str3 = 'buie'
+    expect(exports.isPermutation(str1, str2)).toEqual(true);
+    expect(exports.isPermutation(str1, str3)).toEqual(false);
+  });
+});
 
-        expect(exports.isPermutation(str1, str2)).toEqual(true)
-        expect(exports.isPermutation(str1, str3)).toEqual(false)
-    })
-})
+describe("cleanWhiteSpaces()", () => {
+  it('Should replaces all spaces in a string with "%20"', () => {
+    const str1 = "a b c";
+    const str2 = "a%20b%20c";
 
-describe('cleanWhiteSpaces()', () => {
-    it('Should replaces all spaces in a string with "%20"', () => {
-        const str1 = 'a b c'
-        const str2 = 'a%20b%20c'
+    expect(exports.cleanWhiteSpaces(str1)).toEqual(str2);
+    expect(exports.cleanWhiteSpaces(str1)).toBeTypeOf("string");
+  });
+});
 
-        expect(exports.cleanWhiteSpaces(str1)).toEqual(str2)
-        expect(exports.cleanWhiteSpaces(str1)).toBeTypeOf("string")
-    })
-})
+describe("isPalindromePermutation()", () => {
+  it("Should return true if it is a permutation of a palindrome", () => {
+    const str1 = "Tact Coa";
+    const str2 = "chirpingmermaid";
+    const str3 = "aabc";
+    const str4 = "";
+    expect(exports.isPalindromePermutation(str1)).toEqual(true);
+    expect(exports.isPalindromePermutation(str2)).toEqual(false);
+    expect(exports.isPalindromePermutation(str3)).toEqual(false);
+    expect(exports.isPalindromePermutation(str4)).toEqual(true);
+  });
+});
 
-describe('isPalindromePermutation()', () => {
-    it('Should return true if it is a permutation of a palindrome', () => {
-        const str1 = "Tact Coa"
-        const str2 = "chirpingmermaid"
-        const str3 = "aabc"
-        const str4 = ""
-        expect(exports.isPalindromePermutation(str1)).toEqual(true)
-        expect(exports.isPalindromePermutation(str2)).toEqual(false)
-        expect(exports.isPalindromePermutation(str3)).toEqual(false)
-        expect(exports.isPalindromePermutation(str4)).toEqual(true)
-    })
-})
+describe("oneAway()", () => {
+  it("Should return true if there was less than one change between the strings.", () => {
+    const str1 = "pale";
+    const str2 = "ple";
 
-describe('oneAway()', () => {
-    it('Should return true if there was less than one change between the strings.', () => {
-        const str1 = "pale"
-        const str2 = 'ple'
+    expect(exports.oneAway(str1, str2)).toEqual(true);
+  });
+  it("Should return false if the strings is not one edit away", () => {
+    const str1 = "pale";
+    const str2 = "bake";
+    expect(exports.oneAway(str1, str2)).toEqual(false);
+  });
+});
 
-        expect(exports.oneAway(str1, str2)).toEqual(true)
-    })
-    it('Should return false if the strings is not one edit away', () => {
-        const str1 = "pale"
-        const str2 = "bake"
-        expect(exports.oneAway(str1, str2)).toEqual(false)
-    })
-})
+describe("compressString()", () => {
+  it("Should compress the strings accordingly.", () => {
+    const str1 = "aabcccc";
 
-describe('compressString()', () => {
-    it('Should compress the strings accordingly.', () => {
-        const str1 = "aabcccc"
+    expect(exports.compressString(str1)).toEqual("a2b1c4");
+  });
+  it("Should return original string if the compressed string is not shorter than the original string.", () => {
+    const str2 = "abc";
+    expect(exports.compressString(str2)).toEqual("abc");
+  });
+});
 
-        expect(exports.compressString(str1)).toEqual("a2b1c4")
-    })
-    it ('Should return original string if the compressed string is not shorter than the original string.', () => {
-        const str2 = "abc"
-        expect(exports.compressString(str2)).toEqual("abc")
-    })
-})
-
-describe('rotateMatrix()', () => {
-    it('Should rotate the matrix 90 degrees', () => {
-        let arr = [[1,2,3], [4,5,6], [7,8,9]]
-        let newArr = [[32,42,53], [43,24,64], [23,54, 64]]
-        expect(exports.rotateMatrix(arr)).toEqual([[7,4,1], [8,5,2], [9,6,3]])
-        expect(exports.rotateMatrix(newArr)).toEqual([[23,43,32], [54, 24, 42], [64, 64, 53]])
-    })
-})
+describe("rotateMatrix()", () => {
+  it("Should rotate the matrix 90 degrees", () => {
+    let arr = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    let newArr = [
+      [32, 42, 53],
+      [43, 24, 64],
+      [23, 54, 64],
+    ];
+    expect(exports.rotateMatrix(arr)).toEqual([
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3],
+    ]);
+    expect(exports.rotateMatrix(newArr)).toEqual([
+      [23, 43, 32],
+      [54, 24, 42],
+      [64, 64, 53],
+    ]);
+  });
+});
