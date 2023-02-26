28bf916aa77cae7999b0904f0a6d9678b46f0aa9
Brynner03 <bventura03@hotmail.com>
2023-02-26 18:01:55 -0500

    Cleaned up formatting

--- Data Structures/Strings.js
+++ Data Structures/Strings.js
@@ -3,11 +3,9 @@ const isArrayUnique = (str) => {
     if (str.length == 1) {
         return true
     }
-    
     if (str.length == 0) {
         return false
     }
-    
     for (let i = 0; i < str.length; i++) {
         for (let j = 1; j < str.length; j++) {
             if (str[i] !== str[j]) {
@@ -22,19 +20,15 @@ const isArrayUnique = (str) => {
 }
 
 const isPermutation = (str1, str2) => {
-
     // Base case
     if (str1.length !== str2.length || str1 == null || str2 == null) {
         return false
     }
-
     // Get the map of the first string
     let charMap = getMap(str1)
-
     // Check strB characters and occurrences on the computed map.
     for (let i = 0; i < str1.length; i++) {
         let charB = str2[i]
-
         if (charMap[charB] !== undefined && charMap[charB] >= 1) {
             charMap[charB] --
         } else {
@@ -46,10 +40,8 @@ const isPermutation = (str1, str2) => {
 
 const getMap = (str) => {
     let map = {}
-
     for (let i = 0 ; i < str.length; i++) {
         let currentChar = str[i]
-
         if (!map[currentChar]) {
             map[currentChar] = 1
         } else {
@@ -60,30 +52,22 @@ const getMap = (str) => {
 }
 
 // URLify
-
 const cleanWhiteSpaces = (str) => {
-
     let cleanedString = str.replace(/\s/g, '%20')
     return cleanedString
 }
 
-// Palindrome Permutaion
-
+// Palindrome Permutation
 const isPalindromePermutation = (str) => {
-
     let map = {}
     let normString = str.toLowerCase()
-
     for (let i = 0; i < normString.length; i++) {
-        let currentChar = normString[i]
-        
+        let currentChar = normString[i]   
         if (currentChar !== ' ') {
             map[currentChar] = map[currentChar] ? map[currentChar] += 1 : 1
         }
     }
-
     let hasOddFreq = false
-
     for (const char in map) {
         let freq = map[char]
         if (freq %2 !== 0) {
@@ -94,21 +78,12 @@ const isPalindromePermutation = (str) => {
             }
         }
     }
-
     return true
-
 }
 
-// Return true if the second string is one away or none away
-// Return false if the second string is more than one away.
-
-// hello
-// hell
-
 const oneAway = (str1, str2) => {
-
     const map = {}
-
+    // Loop through the first string and map them
     for (let i = 0; i < str1.length; i++) {
         let currChar = str1[i]
         if (map[currChar]) {
@@ -117,7 +92,10 @@ const oneAway = (str1, str2) => {
             map[currChar] = 1
         }
     }
-    
+    // Loop through the second string and check if it exists in the map. 
+    // If it does, decrement it. 
+    // If it doesn't, store the value.
+    // If it is equal to 0, remove it from the map.
     for (let j = 0; j < str2.length; j++) {
         let str2Char = str2[j]
         if (map[str2Char]) {
@@ -129,20 +107,17 @@ const oneAway = (str1, str2) => {
             delete map[str2Char]
         }
     }
-
-    if (Object.values(map).length > 2) {
+    // If the length of values in the map is more than 1, return false.
+    if (Object.values(map).length >= 2) {
         return false
     } 
         return true
 }
 
 const compressString = (str) => {
-
     let compString = ""
     let count = 1
-
     for (let i = 0; i < str.length; i ++) {
-
         if (str[i] == str[i + 1]) {
             count++
         } else {
@@ -150,50 +125,36 @@ const compressString = (str) => {
             count = 1
         }
     }
-
     if (str.length <= compString.length) {
         return str
     } else {
         return compString
     }
-
 }
 
 const rotateMatrix = (arr) => {
-    
     let left = 0
     let right = arr.length - 1
-
     while( left  < right) {
         for (let i = 0; i < right - left; i++) {
             let top = left 
             let bottom = right
-
             // Save the top left variable
             let topLeft = arr[top][left + i] 
-
             // Move the bottom left into the top left
             arr[top][left + i] = arr[bottom - i][left]
-
             // Move bottom right into bottom left
             arr[bottom - i][left] = arr[bottom] [right - i]
-
             // Move top right into bottom right
             arr[bottom][right - i] = arr[top + i][right]
-
             // Move top left into top right
             arr[top + i][right] = topLeft
         }
         right--
         left++
     }
-
     return arr
-
 }
-
-let arr = [[1,2,3], [4,5,6], [7,8,9]]
-console.log(rotateMatrix(arr))
 
 module.exports = {
     isArrayUnique,
