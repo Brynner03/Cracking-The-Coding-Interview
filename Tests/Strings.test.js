89206ec14378bbd3dae87467247e3a1e2f23eb68
Brynner03 <bventura03@hotmail.com>
2023-02-26 18:07:38 -0500

    Cleaned up imports, no longer have to sinigle-handedly import functions

--- Tests/Strings.test.js
+++ Tests/Strings.test.js
@@ -1,4 +1,4 @@
-import {isArrayUnique, isPermutation, cleanWhiteSpaces, isPalindromePermutation, oneAway, compressString, rotateMatrix} from '../Data Structures/Strings'
+import * as exports from '../Data Structures/Strings'
 import { describe, it, expect } from 'vitest';
 
 
@@ -8,9 +8,9 @@ describe('isArrayUnique()', () => {
         const str2 = 'loo'
         const str3 = 'l'
 
-        expect(isArrayUnique(str1)).toEqual(true)
-        expect(isArrayUnique(str2)).toEqual(false)
-        expect(isArrayUnique(str3)).toEqual(true)
+        expect(exports.isArrayUnique(str1)).toEqual(true)
+        expect(exports.isArrayUnique(str2)).toEqual(false)
+        expect(exports.isArrayUnique(str3)).toEqual(true)
     })
 })
 
@@ -21,8 +21,8 @@ describe('isPermutation()', () => {
 
         const str3 = 'buie'
 
-        expect(isPermutation(str1, str2)).toEqual(true)
-        expect(isPermutation(str1, str3)).toEqual(false)
+        expect(exports.isPermutation(str1, str2)).toEqual(true)
+        expect(exports.isPermutation(str1, str3)).toEqual(false)
     })
 })
 
@@ -31,8 +31,8 @@ describe('cleanWhiteSpaces()', () => {
         const str1 = 'a b c'
         const str2 = 'a%20b%20c'
 
-        expect(cleanWhiteSpaces(str1)).toEqual(str2)
-        expect(cleanWhiteSpaces(str1)).toBeTypeOf("string")
+        expect(exports.cleanWhiteSpaces(str1)).toEqual(str2)
+        expect(exports.cleanWhiteSpaces(str1)).toBeTypeOf("string")
     })
 })
 
@@ -42,10 +42,10 @@ describe('isPalindromePermutation()', () => {
         const str2 = "chirpingmermaid"
         const str3 = "aabc"
         const str4 = ""
-        expect(isPalindromePermutation(str1)).toEqual(true)
-        expect(isPalindromePermutation(str2)).toEqual(false)
-        expect(isPalindromePermutation(str3)).toEqual(false)
-        expect(isPalindromePermutation(str4)).toEqual(true)
+        expect(exports.isPalindromePermutation(str1)).toEqual(true)
+        expect(exports.isPalindromePermutation(str2)).toEqual(false)
+        expect(exports.isPalindromePermutation(str3)).toEqual(false)
+        expect(exports.isPalindromePermutation(str4)).toEqual(true)
     })
 })
 
@@ -54,12 +54,12 @@ describe('oneAway()', () => {
         const str1 = "pale"
         const str2 = 'ple'
 
-        expect(oneAway(str1, str2)).toEqual(true)
+        expect(exports.oneAway(str1, str2)).toEqual(true)
     })
     it('Should return false if the strings is not one edit away', () => {
         const str1 = "pale"
         const str2 = "bake"
-        expect(oneAway(str1, str2)).toEqual(false)
+        expect(exports.oneAway(str1, str2)).toEqual(false)
     })
 })
 
@@ -67,11 +67,11 @@ describe('compressString()', () => {
     it('Should compress the strings accordingly.', () => {
         const str1 = "aabcccc"
 
-        expect(compressString(str1)).toEqual("a2b1c4")
+        expect(exports.compressString(str1)).toEqual("a2b1c4")
     })
     it ('Should return original string if the compressed string is not shorter than the original string.', () => {
         const str2 = "abc"
-        expect(compressString(str2)).toEqual("abc")
+        expect(exports.compressString(str2)).toEqual("abc")
     })
 })
 
@@ -79,7 +79,7 @@ describe('rotateMatrix()', () => {
     it('Should rotate the matrix 90 degrees', () => {
         let arr = [[1,2,3], [4,5,6], [7,8,9]]
         let newArr = [[32,42,53], [43,24,64], [23,54, 64]]
-        expect(rotateMatrix(arr)).toEqual([[7,4,1], [8,5,2], [9,6,3]])
-        expect(rotateMatrix(newArr)).toEqual([[23,43,32], [54, 24, 42], [64, 64, 53]])
+        expect(exports.rotateMatrix(arr)).toEqual([[7,4,1], [8,5,2], [9,6,3]])
+        expect(exports.rotateMatrix(newArr)).toEqual([[23,43,32], [54, 24, 42], [64, 64, 53]])
     })
 })
